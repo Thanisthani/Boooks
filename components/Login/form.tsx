d8548fe79b823a5c37449fd0dae595bf2eb3b30e
Thanistas <thanithanis171198@gmail.com>
2022-10-11 12:14:55 +0530

    Build login

---
+++ components/Login/form.tsx
@@ -0,0 +1,75 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const LoginForm = () => {
+
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password:''
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        },
+        validationSchema: yup.object({
+          email: yup
+            .string()
+            .email('Must be a valid email')
+            .required('Email is required'),
+          password: yup.string().trim().min(8, "Must be 8 characters or more").required('Password is required'),
+        }),
+    });
+    
+
+    return ( 
+        <div className='pt-10'>
+            <form className="w-50" onSubmit={formik.handleSubmit}>
+
+        <div className="mb-3 flex flex-col">
+          <label htmlFor="email" className="text-lg text-[#50555c] font-semibold pb-3">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="mt-1 block w-[350px] px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none  focus:ring-[#d29bdb] focus:border-[#d29bdb] focus:ring-2 hover:border-[#d29bdb]"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.errors.email && (
+            <div className="text-red-700">{formik.errors.email}</div>
+          )}
+        </div>
+
+        <div className="mb-3 flex flex-col">
+          <label htmlFor="name" className="text-lg font-semibold text-[#50555c] pb-3">
+            Password
+          </label>
+          <input
+            type="text"
+            name="password"
+            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none  focus:ring-[#d29bdb] focus:border-[#d29bdb] focus:ring-2 hover:border-[#d29bdb]"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+                    />
+                    
+                    {/*   */}
+          {formik.errors.password && (
+            <div className="text-red-700">{formik.errors.password}</div>
+          )}
+        </div>
+   
+
+        <button type="submit" className="bg-[#592E64] w-full text-white py-2 mt-8 rounded-md text-lg font-semibold hover:bg-[#c37acf]">
+         Login
+        </button>
+      </form>
+        </div>
+     );
+}
+ 
+export default LoginForm;
---
+++ pages/login.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import LoginForm from '../components/Login/form';
+import book from '/public/asset/login.jpg';
+import logo from '/public/asset/Boooks_logo.png';
+
+const login = () => {
+    return ( 
+        <div className="bg-[#592E64]  h-screen w-screen py-20 px-64">
+            <div className="bg-[#fefefe] h-full w-full rounded-xl flex ">
+                
+                <div className='w-1/2 pt-10 ml-10'>
+                <Image src={logo} alt="logo" height={35} className="cursor-pointer object-contain" />
+                <div className="  flex flex-col items-center justify-center pt-10">
+          
+                    <p className="font-thin text-4xl">Welcome Back</p>
+
+                    <LoginForm />
+                    </div>
+                    
+                    </div>
+
+                <div>
+                    <div className='flex space-x-5 justify-end pt-10 font-semibold'>
+                        <p className='text-[#50555c]'>Don't have an account ? </p> 
+                        <Link href="">
+                        <p className='text-[#592E64] '>Sign Up</p>
+                        </Link>
+                    </div>
+                    
+                <Image src={ book} width={400} height={450} className='object-contain' />
+                </div>
+
+
+            </div>
+            
+        </div>
+     );
+}
+ 
+export default login;
